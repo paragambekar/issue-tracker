3d1bf3b8379c7adab798a9e6d51fe6fc627c3ae9
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-29 18:01:25 +0530

    filter bug fixed

--- controllers/projects-controller.js
+++ controllers/projects-controller.js
@@ -135,25 +135,127 @@ module.exports.search= async function(request, response){
 
     }
 
-    if(issueList.length == 0){
-        for(issue of product){
-            issueList.push(issue._id);
+    let filLabel = [];
+    // by labels
+    for(let i= 0; i< product.length; i++){
+        // filter by labels 
+        if(request.body.labels){
+            let labelArray = [];
+            if(typeof(request.body.labels) == 'string'){
+                let label = request.body.labels;
+                labelArray.push(label);
+            }else{
+                for(let label of request.body.labels){
+                    labelArray.push(label);
+                }
+            }
+            let currLabelsArray = product[i].labels;
+
+            let result = labelArray.every(label => currLabelsArray.includes(label));
+            if(result){
+                if(!filLabel.includes(product[i]._id)){
+                    filLabel.push(product[i]._id); 
+                }
+            }
         }
     }
+
+    console.log('filterLabel---------->',filLabel);
+    let popFilLabel = [];
+    for(issue of filLabel){
+        let iss = await Issue.findById(issue).populate();
+        popFilLabel.push(iss);
+    }
+    console.log('PopulatedfilterLabel---------->',popFilLabel);
+
+
+    let authr = [];
+    let popAuthor = [];
+    if(request.body.author == ''){
+        popAuthor = Array.from(popFilLabel);
+        authr = Array.from(filLabel);
+    }else{
+            for(let i= 0; i< popFilLabel.length; i++){
+            
+                let currIssue = popFilLabel[i];
+                // filter by author 
+                if(request.body.author != ''){
+                    if(popFilLabel[i].author === request.body.author){
+                        authr.push(popFilLabel[i]._id);   
+                }
+            }
+        }
+        for(issue of authr){
+            let iss = await Issue.findById(issue).populate();
+            popAuthor.push(iss);
+        }
+
+    }
+    console.log('Author------->',authr);
+    console.log('PopopopoAuthor------->',popAuthor);
+
+    let titleDes = [];
+    let popTitleDes = [];
+    if(request.body.title == ''){
+        popTitleDes = Array.from(popAuthor);
+        titleDes = Array.from(authr);
+    }else{
+        for(let i= 0; i< popAuthor.length; i++){
+            if(request.body.title){
+                const reqTitle = request.body.title;
+                const regEx = new RegExp(`${reqTitle}`,'gi');
+                if(popAuthor[i].title.match(regEx) || popAuthor[i].description.match(regEx)){
+                    if(!titleDes.includes(popAuthor[i]._id)){
+                        titleDes.push(popAuthor[i]._id); 
+                    }
+                }
+            }
+        }
+        for(issue of titleDes){
+            let iss = await Issue.findById(issue).populate();
+            popTitleDes.push(iss);
+        }
+
+    }
+
+    console.log('PopopopoTitleDes------->',popTitleDes);
+    console.log('TitleDes------->',titleDes);
+
+    if(!request.body.labels){
+        if(!request.body.author){
+            if(!request.body.title){
+                console.log('NOthing insdie request');
+                for(issue of product){
+                    titleDes.push(issue._id);
+                }
+
+                for(issue of titleDes){
+                    let iss = await Issue.findById(issue).populate();
+                    popTitleDes.push(iss);
+                }
+            }
+        }
+    }
+    
+    // if(titleDes.length == 0){
+    //     for(issue of product){
+    //         issueList.push(issue._id);
+    //     }
+    // }
 
     let issueArray = [];
     for(let i of issueList){
         let iss = await Issue.findById(i).populate();
         issueArray.push(iss);
     }
-
+    console.log("Request body---------->", request.body);
     // handle ajax requests 
     if(request.xhr){
         return response.status(200).json({
             data : {
                 project : project,
-                issueArray : issueArray, 
-                issueList : issueList,
+                issueArray : popTitleDes, 
+                issueList : titleDes,
             },
             message : "Issue created by ajax",
         });
